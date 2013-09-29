17eea9b4ce4ebbeaa15fc76940464225dedaa606
AJ <sevaine@gmail.com>
2013-09-29 17:24:38 +1000

    refactored in pam_authtok as per the OpenPam
    sample module in http://www.freebsd.org/doc/en/articles/pam/pam-sample-module.html

--- src/pam_authvia_http_formpost.c
+++ src/pam_authvia_http_formpost.c
@@ -6,31 +6,87 @@
 * credits           - Based on 2ndfactor.c pam module for 2 factor auth from
 *                     http://ben.akrin.com/2FA
 *******************************************************************************/
-#include <stdio.h>
+#include <pwd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
-/* Expected hook */
-PAM_EXTERN = int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-  return PAM_SUCCESS;
-}
+#ifndef _OPENPAM
+static char password_prompt[] = "Password:";
+#endif
 
-/* expected hook for our custom actions */
-PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-  int retval;
-  int i;
+#ifndef PAM_EXTERN
+#define PAM_EXTERN
+#endif
 
-  /* for converse() */
-  char *input;
-  struct pam_message msg[1],*pmsg[1];
+PAM_EXTERN int
+pam_sm_authenticate( pam_handle_t *pamh, int flags,i int argc, const char *argv[] ) 
+{
+#ifndef _OPENPAM
+  struct pam_conf *conv;
+  struct pam_message msg;
+  const struct pam_message *msgp;
   struct pam_response *resp;
+#endif
+  struct passwd *pwd;
+  const char *username;
+  char *crypt_password, *password;
+  int pam_err, retry;
 
-  ./* parameters */
-  int got_base_url = 0;
-  int got_api_key = 0;
+  if (( pam_err = pam_get_user(pamh,&user, NULL)) != PAM_SUCCESS)
+  {
+    return (pam_err);
+  }
+
+#ifndef _OPENPAM
+  pam_err = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
+  if (pam_err != PAM_SUCCESS)
+  {
+    return(PAM_SYSTEM_ERR);
+  }
+  msg.msg_style = PAM_PROMPT_ECHO_OFF;
+  msg.msg       = password_prompt;
+  msgp          = &msg;
+#endif
+  for( retry = 0; retry < 3 ; ++retry )
+  {
+#ifdef _OPENPAM
+    pam_err     = pam_get_authtok(pamh,PAM_AUTHTOK,(const char **)&password,NULL);
+#else
+    resp        = NULL;
+    pam_err     = (*conv->conv)(1, &msgp, &resp, conv->appdata_ptr);
+    if ( resp != NULL )
+    {
+      if ( pam_err == PAM_SUCCESS )
+      {
+        password = resp->resp;
+      }
+      else
+      {
+        free(resp->resp);
+      }
+      free(resp);
+    }
+#endif
+    if ( pam_err == PAM_SUCCESS ) 
+    {
+      break;
+    }
+  }
+  if ( pam_err == PAM_SUCCESS )
+  {
+    return pam_err;
+  }
+  if ( pam_err != PAM_SUCCESS )
+  {
+    return PAM_AUTH_ERR;
+  }
+
+  int got_base_url    = 0;
+  int got_api_key     = 0;
   char base_url[256];
   char api_key[256];
   for( i=0 ; i<argc ; i++ ) {
@@ -46,21 +102,12 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     return PAM_AUTH_ERR;
   }
 
-  /* username that was used in the previous authentication */
-  const char *username;
-  if( (retval = pam_get_user(pamh,&username,"login: "))!=PAM_SUCCESS ) {
-    return retval ;
-  }
-        
-  /* Build the url */
-  char post_params[ strlen("username=") + strlen("?password=") + strlen(password) + strlen("?auth_key=") + strlen(auth_key) + strlen("?ip=") + strlen(ip) ];
-  strcpy( post_params, base_url );
+  char post_params[ strlen("username=") + strlen("?password=") + strlen(password) + strlen("?auth_key=") + strlen(auth_key) ];
   strcpy( post_params, "?username=" );
   strcpy( post_params, username );
   strcpy( post_params, "?password=" );
   strcpy( post_params, password );
   strcpy( post_params, "?auth_key=" );
   strcpy( post_params, auth_key );
-  strcpy( post_params, "?ip=" );
-  strcpy( post_params, ip );
+
 
